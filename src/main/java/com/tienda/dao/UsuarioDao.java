ec5c78d23e177c5c5d690395f4b195c86ef73de6
Joaquin Barrantes <joaquin.barrantes@ni.com>
2023-11-23 18:37:58 -0600

    Semana 11

--- src/main/java/com/tienda/dao/UsuarioDao.java
+++ src/main/java/com/tienda/dao/UsuarioDao.java
@@ -9,4 +9,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UsuarioDao extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String password);
+    
+    Usuario findByUsernameOrCorreo(String username, String correo);
+    
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
